9f7f761467492475f6b67293c020f6051ea3ef39
Katie Fujihara and Kayla Ondracek <katie@>
2018-04-04 13:41:21 -0700

    add ability to add new projects to database

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,5 +1,5 @@
 export class Project {
   goalMet: boolean = false;
-  
-  constructor(name: string, starters: string[], description: string, goal: number, plans: string, rewards: string[]){}
+
+  constructor(public name: string, public starters: string[], public description: string, public goal: number, public plans: string, public rewards: string[]){}
 }
--- src/app/project-new/project-new.component.ts
+++ src/app/project-new/project-new.component.ts
@@ -2,21 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { ProjectsService } from '../projects.service';
 import { Router } from '@angular/router';
+import { Project } from '../models/project.model';
 
 @Component({
   selector: 'app-project-new',
   templateUrl: './project-new.component.html',
-  styleUrls: ['./project-new.component.css']
+  styleUrls: ['./project-new.component.css'],
+  providers: [ProjectsService]
 })
 export class ProjectNewComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private projectsService: ProjectsService) { }
 
   ngOnInit() {
   }
 
-  addProject(name: string, starters: string, description: string, goal: string, plans: string, rewards: string) {
-    
+  addProject(projectName: string, projectStarters: string, projectDescription: string, projectGoal: string, projectPlans: string, projectRewards: string) {
+    let starterArray = projectStarters.split(", ");
+    let rewardsArray = projectRewards.split(", ");
+
+
+    this.projectsService.addProject(new Project(projectName, starterArray, projectDescription, parseInt(projectGoal), projectPlans, rewardsArray));
   }
 
 }
