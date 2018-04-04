0043d23a08a4e26430ad7b67fcca337ff9b27ffe
Katie Fujihara and Kayla Ondracek <katie@>
2018-04-04 09:39:55 -0700

    add initial files, welcome component, home route, initial connection to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routing } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MasterFireBase } from './api-keys'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
+export const firebaseConfig = {
+  apiKey: MasterFireBase.apiKey,
+  authDomain:  MasterFireBase.authDomain,
+  databaseURL:  MasterFireBase.databaseURL,
+  storageBucket:  MasterFireBase.storageBucket
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProjectListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/project.model.ts
@@ -0,0 +1,5 @@
+export class Project {
+  goalMet: boolean = false;
+  
+  constructor(name: string, starters: string[], description: string, goal: number, plans: string, rewards: string[]){}
+}
