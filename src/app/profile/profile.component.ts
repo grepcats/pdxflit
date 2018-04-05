8fe4be29edd69c50a2bfcc8df3d496d56e514054
Katie Fujihara and Kayla Ondracek <katie@>
2018-04-05 15:58:33 -0700

    fix race condition bug

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,19 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
 import { FirebaseListObservable } from 'angularfire2/database';
-
+import { AuthenticationService } from '../authentication.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
-  providers: [UserService]
+  providers: [AuthenticationService, UserService]
 })
 export class ProfileComponent implements OnInit {
-  user;
-  constructor(private router: Router, private userService: UserService) { }
+  userUID;
+  databaseUser;
+  constructor(private router: Router, private userService: UserService, public authService: AuthenticationService) {
 
+  }
 
   ngOnInit() {
-  }
-}
+    this.authService.user.subscribe(user => {
+      if (user != null) {
+        this.userUID = user.uid;
+
+      } else {
+        console.log("you logged out");
+      }
+    })
+
+    this.userService.getUserByUID(this.userUID).subscribe(dataLastEmittedFromObserver => {
+    this.databaseUser = dataLastEmittedFromObserver[0];)
+
+
+
+
+  });
