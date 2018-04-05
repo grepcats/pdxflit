719077b0c9b831046bcfe14a5496eca705db0325
Katie Fujihara and Kayla Ondracek <katie@>
2018-04-05 11:03:58 -0700

    create user class

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+export class User {
+  rewardsEarned: string[];
+  volunteerOps: string[];
+  projectsSupported: string[];
+
+  constructor(
+    public name: string,
+    public username: string
+  ){}
+
+
+}
